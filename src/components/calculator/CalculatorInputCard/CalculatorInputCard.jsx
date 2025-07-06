1faa6ea601fc58bbaea98350eb6bcd0ac3b9da40
rhordiichuk <rhordiichuk@gmail.com>
2025-07-06 21:15:53 +0300

    feat: working with BMI calculator - added calculator cards - added brief info - added results explanation info

---
+++ src/components/calculator/CalculatorInputCard/CalculatorInputCard.jsx
@@ -0,0 +1,44 @@
+import styles from './CalculatorInputCard.module.css'
+
+export default function CalculatorInputCard() {
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.header}>
+        Adult BMI Calculator
+      </h2>
+      <main className={styles.main}>
+        <div className={styles.genderButtonsContainer}>
+          <button className={styles.maleButton}><span class="material-symbols-outlined">
+            male
+          </span>
+            Male
+          </button>
+          <button className={styles.femaleButton}>
+            <span class="material-symbols-outlined">
+              female
+            </span>
+            Female
+          </button>
+        </div>
+        <div className={styles.inputsContainer}>
+          <div>
+            <div className={styles.inputLabel}>Age</div>
+            <div><input type="text" /> years ( yrs )</div>
+          </div>
+          <div>
+            <div className={styles.inputLabel}>Height</div>
+            <div><input type="text" /> centimeters ( cm )</div>
+          </div>
+          <div>
+            <div className={styles.inputLabel}>Weight</div>
+            <div><input type="text" /> kilograms ( kg )</div>
+          </div>
+        </div>
+        <div className={styles.actionButtons}>
+          <button className={styles.resetButton}>Reset</button>
+          <button className={styles.calculateButton}>Calculate</button>
+        </div>
+      </main>
+    </div>
+  )
+}
--- src/pages/CalculatorBMI/CalculatorBMI.jsx
+++ src/pages/CalculatorBMI/CalculatorBMI.jsx
@@ -1,12 +1,60 @@
 import styles from './CalculatorBMI.module.css';
 import NewsLetter from '@components/home/NewsLetter/NewsLetter';
+import rectangle from '@assets/images/rectangle.png';
+import CalculatorInputCard from '../../components/calculator/CalculatorInputCard/CalculatorInputCard';
+import CalculatorOutputCard from '../../components/calculator/CalculatorOutputCard/CalculatorOutputCard';
 
 export default function CalculatorBMI() {
   return (
     <div className={styles.container}>
-      <div>gray box</div>
-      <div>calculator block</div>
-      <div>what your BMI means</div>
+      <div className={styles.grayBox}></div>
+      <div className={styles.mainContainer}>
+        <div className={styles.infoContainer}>
+          <div>
+            <h2 className={styles.header}>BMI Calculator</h2>
+            <div className={styles.infoText}>
+              <span style={{ marginTop: '24px', display: 'inline-block' }}>
+                <b>Body mass index (BMI)</b> is a measure of body fat based on height and weight that applies to adult men and women. Your BMI is just one piece of the puzzle. It’s based on height and weight but doesn’t take into account your muscle mass, bone density, or body composition. Your healthcare provider will consider whether your BMI is too high or too low for you.
+              </span>
+              <span style={{ marginTop: '1lh', display: 'inline-block' }}>
+                At our organic store we use your BMA to help recommend products that best support your body's current needs. Whether you're aiming to lose weight build straingth or maintain balance we offer clean nutrient-reach foodstailored to your wellness journey because real house starts with the right fuel for your body
+              </span>
+            </div>
+          </div>
+          <div>
+            <img src={rectangle} alt="mock gray block image" />
+          </div>
+        </div>
+
+        <div className={styles.calculatorContainer}>
+          <div>
+            <h2 className={styles.header}>Calculate your body mass index (BMI) for adults</h2>
+          </div>
+          <div className={styles.calculatorCardsContainer}>
+            <CalculatorInputCard />
+            <CalculatorOutputCard />
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.resultExplanations}>
+        <div>
+          <h2 className={styles.header}>What your BMI results means</h2>
+          <div className={styles.infoText}>
+            <span style={{ marginTop: '24px', display: 'inline-block' }}>
+              A BMI score of between 25 and 29.9 is classed as 'overweight'.
+              This category of BMI is associated with a higher risk of health conditions, such as, heart disease and certain types of cancer.
+              Healthy weight loss
+            </span>
+            <span style={{ marginTop: '1lh', display: 'inline-block' }}>
+              If you want to lose weight, a good approach is to reduce your calorie intake by around 600 calories per day. This can be achieved by reducing high calorie foods such as sugary drinks, and ultra processed foods such as biscuits and crisps. Consuming plenty of fibre and protein will keep you fuller for longer and help to prevent overeating. Managing stress and sleeping well can also make it easier to eat well. Increasing your physical activity has many benefits and can help to you to use more energy each day.
+            </span>
+          </div>
+        </div>
+        <div>
+          <img src={rectangle} alt="mock gray block image" />
+        </div>
+      </div>
       <div>showcase block</div>
       <NewsLetter />
     </div>
