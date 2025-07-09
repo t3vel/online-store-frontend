0daf083dffe834ba32cf9842b2959d854179c162
rhordiichuk <rhordiichuk@gmail.com>
2025-07-09 22:28:27 +0200

    SCRUM-104 (feat): finished BMI calculator page

--- src/components/calculator/CalculatorInputCard/CalculatorInputCard.jsx
+++ src/components/calculator/CalculatorInputCard/CalculatorInputCard.jsx
@@ -1,37 +1,38 @@
-import styles from './CalculatorInputCard.module.css'
+import styles from "./CalculatorInputCard.module.css";
 
 export default function CalculatorInputCard() {
   return (
     <div className={styles.container}>
-      <h2 className={styles.header}>
-        Adult BMI Calculator
-      </h2>
+      <h2 className={styles.header}>Adult BMI Calculator</h2>
       <main className={styles.main}>
         <div className={styles.genderButtonsContainer}>
-          <button className={styles.maleButton}><span class="material-symbols-outlined">
-            male
-          </span>
+          <button className={styles.maleButton}>
+            <span className="material-symbols-outlined">male</span>
             Male
           </button>
           <button className={styles.femaleButton}>
-            <span class="material-symbols-outlined">
-              female
-            </span>
+            <span className="material-symbols-outlined">female</span>
             Female
           </button>
         </div>
         <div className={styles.inputsContainer}>
           <div>
             <div className={styles.inputLabel}>Age</div>
-            <div><input type="text" /> years ( yrs )</div>
+            <div>
+              <input type="number" /> years ( yrs )
+            </div>
           </div>
           <div>
             <div className={styles.inputLabel}>Height</div>
-            <div><input type="text" /> centimeters ( cm )</div>
+            <div>
+              <input type="text" /> centimeters ( cm )
+            </div>
           </div>
           <div>
             <div className={styles.inputLabel}>Weight</div>
-            <div><input type="text" /> kilograms ( kg )</div>
+            <div>
+              <input type="text" /> kilograms ( kg )
+            </div>
           </div>
         </div>
         <div className={styles.actionButtons}>
@@ -40,5 +41,5 @@ export default function CalculatorInputCard() {
         </div>
       </main>
     </div>
-  )
+  );
 }
